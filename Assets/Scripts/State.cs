e5a3dd0707341eba0196cd02486f4ecee07f7e5d
RyanPortfolioGit2199 <ryan.allen.2199@gmail.com>
2025-05-23 15:04:59 -0700

    State Tutorial part 1

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//When creating a Script in Unity it defaults to a monobehavior which is a script to be placed on a Gameobject. States dont need a Monobehavior so get rid of it but that also takes away the start and update functions.
+public abstract class State   // An abstract class is a restricted class that cannot be used to create objects(to access it, it must be inherited from another class)
+{
+
+}
